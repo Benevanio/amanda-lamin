9d17204942e21d9ea6fab9bb7780be5446580106
Benevanio dos Santos Conceicao <benevanio.conceicao@sysmap.com.br>
2024-10-22 18:43:00 -0300

    Add media queries for responsive design in Hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,21 +4,28 @@ import Styles from './Hero.module.css';
 
 export const Hero = () => {
   return (
-  <section className={Styles.container}>
-    <div className={Styles.content}>
-      <h1 className={Styles.title}>
-      Bem-vindo ao Meu Consultório Virtual
-      </h1>
-      <p className={Styles.description}>
-       Sou Amanda Lamin, psicanalista especializada em atendimentos individuais e
-       rodas de conversa voltadas  para empresas.
-       Meu objetivo é promover a saúde mental e o bem-estar emocional de cada pessoa.
-       </p>
-       <a href="amandamlamim@gmail.com">Entre em contato!!</a>
-    </div>
-   < img src={getImagURL('logo.png')} alt="hero"  />
-   <div className={Styles.topBlur} />
-    <div className={Styles.bottomBlur} />
-  </section>
+    <section className={Styles.container}>
+      <div className={Styles.content}>
+        <h1 className={Styles.title}>
+          Bem-vindo ao Meu Consultório Virtual
+        </h1>
+        <p className={Styles.description}>
+          Sou Amanda Lamin, psicanalista especializada em atendimentos individuais e
+          rodas de conversa voltadas para empresas.
+          Meu objetivo é promover a saúde mental e o bem-estar emocional de cada pessoa.
+        </p>
+        
+        <a 
+          href="https://wa.me/5548991151734" 
+          className={Styles.contactButton} 
+          target="_blank" 
+          rel="noopener noreferrer" 
+        >
+          Entre em contato!!
+        </a>
+      </div>
+      <img src={getImagURL('1.png')} alt="hero" className={Styles.heroIMG} />
+      <div className={Styles.bottomBlur} />
+    </section>
   );
 };
